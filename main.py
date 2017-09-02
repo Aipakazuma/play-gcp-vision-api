38224595de496afa8b3703344c0ea3d14559c8df
commanderaipa <gundamgundam777@gmail.com>
2017-09-02 16:13:18 +0900

    [modify] もうちょっとnumpy <-> pil <-> python file object間の変換やりやすく高速化できないのだろうか...

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 import copy
 import io
 import time
+import cv2
 
 
 def request_api(img):
@@ -29,6 +30,10 @@ def request_api(img):
 
 
 def response_parser(res):
+    if 'error' in res['responses'][0]:
+        print(res)
+        assert False
+
     text_annotations = res['responses'][0]['textAnnotations']
 
     detect_list = []
@@ -47,12 +52,30 @@ def draw(img, detect_list):
     for detect in detect_list:
         lt = detect[1][0]
         rb = detect[1][2]
+        x_conditions = 'x' in lt and 'x' in rb
+        y_conditions = 'y' in rb and 'y' in rb
+        if not(x_conditions and y_conditions): continue
+
         lt_point = lt['x'], lt['y']
         rb_point = rb['x'], rb['y']
         draw_img.rectangle((lt_point, rb_point), outline=(150, 0, 0), fill=(150, 0, 0))
 
     c_img.save('./assets/%s.jpg' % (time.time()))
     c_img.show()
+
+
+def preprocessing(img):
+    pil_img = Image.open(io.BytesIO(img))
+    print(len(pil_img.tobytes()))
+    img_np = np.asarray(pil_img)
+    gray_img = cv2.cvtColor(img_np, cv2.COLOR_RGB2GRAY)
+    
+    kernel = np.ones([5, 5], np.uint8)
+    dilate = cv2.dilate(gray_img, kernel, iterations=1)
+
+    sub = 255 - (dilate - gray_img)
+
+    return {'dilate': dilate, 'sub': sub}
 
 
 def main():
@@ -62,9 +85,16 @@ def main():
     with open(img_path, 'rb') as f:
         img = f.read()
 
-    res = request_api(img)
-    detect_list = response_parser(res)
-    draw(img, detect_list)
+    print(len(img))
+    img_dict = preprocessing(img)
+    buffer = io.BytesIO()
+    Image.fromarray(img_dict['sub']).save(buffer, 'jpeg')
+    request_img = buffer.getvalue()
+    print(len(request_img))
+
+    # res = request_api(request_img)
+    # detect_list = response_parser(res)
+    # draw(img, detect_list)
 
 
 if __name__ == '__main__':
