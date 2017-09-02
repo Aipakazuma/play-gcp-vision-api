ea7900434a6e201a34c3c3050b8eb568a8b2b95f
commanderaipa <gundamgundam777@gmail.com>
2017-09-02 14:46:02 +0900

    [modify] 検出位置を画像に表示するように修正.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import base64
 from config import config
 import json
+import copy
+import io
+import time
 
 
 def request_api(img):
@@ -37,7 +40,21 @@ def response_parser(res):
 
     return detect_list
 
-    
+
+def draw(img, detect_list):
+    c_img = Image.open(io.BytesIO(img))
+    draw_img = ImageDraw.Draw(c_img)
+    for detect in detect_list:
+        lt = detect[1][0]
+        rb = detect[1][2]
+        lt_point = lt['x'], lt['y']
+        rb_point = rb['x'], rb['y']
+        draw_img.rectangle((lt_point, rb_point), outline=(150, 0, 0), fill=(150, 0, 0))
+
+    c_img.save('./assets/%s.jpg' % (time.time()))
+    c_img.show()
+
+
 def main():
     img_path = './assets/1.jpg'
 
@@ -47,6 +64,7 @@ def main():
 
     res = request_api(img)
     detect_list = response_parser(res)
+    draw(img, detect_list)
 
 
 if __name__ == '__main__':
